20c1a67cf4010672f403c824e067e1acb2d702d8
3x2-1 <106444541+3x2-1@users.noreply.github.com>
2022-10-17 19:34:15 +0200

    Update and rename TestLogin.sh to Gretings.sh

---
+++ Gretings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello What is your name?"
+
+read name
+
+echo "Hello $name! You're Awesome!!"
--- TestLogin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-For $num in 100 do
-      Echo -ne $num + %
-      $num + 1
-Done
